c730642882ba4cdde2a2994d4779cfd2257d222a
SirCream <kyleprudencio@Davids-MacBook-Air-4.local>
2017-05-16 11:55:36 -0400

    z

--- FinalProject/src/PFB.java
+++ FinalProject/src/PFB.java
@@ -10,7 +10,7 @@ public class PFB
 		answer = 1 + (int)(Math.random() * 999);
 		while (answer > 0)
 		{
-			digits.add(answer % 10, 0);
+			digits.add(0, answer % 10);
 			answer /= 10;
 		}
 		guessCount = 0;
@@ -28,7 +28,6 @@ public class PFB
 		while (x == 1)
 		{
 			int pfb = keyboard.nextInt();
-			guess(pfb);
 			if (guess(pfb).equals("Congratulations! You guessed it!" + '\n' + "You took " + guessCount + " guesses."))
 				x = 0;
 		}
@@ -45,28 +44,30 @@ public class PFB
 		ArrayList <Integer> temp2 = digits;
 		while (a > 0)
 		{
-			temp.add(a % 10, 0);
+			temp.add(0, a % 10);
 			a /= 10;
 		}
-		for (int i = temp.size() - 1; i >= 0; i --) // checks for same digit in same position
-			if (temp2.get(i) == temp.get(i))
-			{
-				temp.remove(i);
-				temp2.remove(i);
-				ans += "P";
-			}	
-		for (int i = temp.size() - 1; i >= 0; i --) // checks for same digit in different position
-			if (temp2.get(i) == temp.get(i + 1))
-			{
-				temp.remove(i + 1);
-				temp2.remove(i);
-				ans += "F";
-			}
-		for (int i = temp.size() - 1; i >= 0; i --) // counts unique digits
-		{
-			temp.remove(i);
-			ans += "B";
-		}
+		System.out.println(temp.size());
+		
+//		for (int i = temp.size() - 1; i >= 0; i --) // checks for same digit in same position
+//			if (temp2.get(i) == temp.get(i))
+//			{
+//				temp.remove(i);
+//				temp2.remove(i);
+//				ans += "P";
+//			}	
+//		for (int i = temp.size() - 1; i >= 0; i --) // checks for same digit in different position
+//			if (temp2.get(i) == temp.get(i + 1))
+//			{
+//				temp.remove(i + 1);
+//				temp2.remove(i);
+//				ans += "F";
+//			}
+//		for (int i = temp.size() - 1; i >= 0; i --) // counts unique digits
+//		{
+//			temp.remove(i);
+//			ans += "B";
+//		}
 		return ans;
 	}
 }
