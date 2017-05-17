732869f3c7c94189a01954605a3ff6524e733571
SirCream <ethanfowler@MacBook-Pro.local>
2017-05-17 15:35:24 -0400

    335

--- FinalProject/src/TicTacToe.java
+++ FinalProject/src/TicTacToe.java
@@ -1,21 +1,26 @@
 
 public class TicTacToe {
 	
-	static String[][] table = new String[3][3];
+	static char[][] table = new char[3][3];
 	
 	public static void main(String args[]){
+		table();
 		printBoard();
 	}
 	
-	public static void printBoard(){
-		System.out.println("0" + table[0][0] + "|" + table[0][1] + "|" + table[0][2]);
-	    System.out.println("-------");
-	    System.out.println("1" + table[1][0] + "|" + table[1][1] + "|" + table[1][2]);
-	    System.out.println("-------");
-	    System.out.println("2" + table[2][0] + "|" + table[2][1] + "|" + table[2][2]);
-	    System.out.println("0 1 2 ");
-		
+	public static void table(){
+	    for (int i = 0; i < 3; i++)
+	        for (int p=0; p < 3; p++)
+	            table [i][p]= ' ';
 	}
 	
-
+	public static void printBoard(){
+		System.out.println("  0  " + table[0][0] + " | " + table[0][1] + " | " + table[0][2]);
+	    System.out.println("     --+---+--");
+	    System.out.println("  1  " + table[1][0] + " | " + table[1][1] + " | " + table[1][2]);
+	    System.out.println("     --+---+--");
+	    System.out.println("  2  " + table[2][0] + " | " + table[2][1] + " | " + table[2][2]);
+	    System.out.println("     0   1   2 ");
+		
+	}
 }
